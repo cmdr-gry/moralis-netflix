c17360fcd677b8a48726f08643891d7e2ff9dd04
Griff <gryphogrypho@gmail.com>
2022-04-05 19:58:36 +0300

    Add files via upload

---
+++ src/helpers/library.js
@@ -0,0 +1,175 @@
+import titanicS from "../images/titanicScene.png";
+import tita from "../images/tit.png";
+import movieLogo from "../images/movieLogo.png";
+import spidey from "../images/spidey.png";
+import jumanji from "../images/jumanji.png";
+import wolf from "../images/wolf.png";
+import mrs from "../images/mrs.png";
+import hob from "../images/hob.png";
+import spideyLogo from "../images/spideyLogo.png";
+import spideyScene from "../images/spideyScene.png";
+import moralis from "../images/moralis.png";
+import moralisLogo from "../images/moralisScene.png";
+import jam from "../images/jam.png";
+import jamLogo from "../images/jamLogo.png";
+import jamScene from "../images/jamScene.png";
+import bayw from "../images/bayw.png";
+import shrek from "../images/shrek.png";
+import lucy from "../images/lucy.png";
+import topgu from "../images/topgu.png";
+import imit from "../images/imit.png";
+import reven from "../images/reven.png";
+import tomb from "../images/tomb.png";
+import amaz from "../images/amaz.png";
+import bigs from "../images/bigs.png";
+import secona from "../images/bigs.png";
+import ted from "../images/ted.png";
+import amerp from "../images/amerp.png";
+
+
+
+export const movies = [
+    {
+        Movie: "https://ipfs.moralis.io:2053/ipfs/QmasHxsKWWNmbKicSdDWVBmVkmgnaTxMp7Cn4YsyPRufgk/media/0",
+        Thumbnail: tita,
+        Scene: titanicS,
+        Logo: movieLogo,
+        Name: "Titanic",
+        Description: "Starring Leonardo DiCaprio and Kate Winslet.",
+        Genre: "Romance, Drama",
+        Actors: "Kate Winslet, Leonardo DiCaprio",
+        Year: "1998",
+        Duration: "3h 14min",
+    },
+    {
+        Movie: "",
+        Thumbnail: spidey,
+        Scene: spideyScene,
+        Logo: spideyLogo,
+        Name: "Spiderman Homecoming",
+        Description: "Peter Parker tries to stop the Vulture from selling weapons.",
+        Genre: "Action, Superhero, Comedy",
+        Actors: "Tom Holland, Zendeya",
+        Year: "2017",
+        Duration: "2h 13min",
+    },
+    {
+        Movie: "",
+        Thumbnail: jumanji,
+        Scene: "",
+        Logo: "",
+        Name: "",
+        Description: "",
+        Genre: "",
+        Actors: "",
+        Year: "",
+        Duration: "",
+    },
+    {
+        Movie: "",
+        Thumbnail: ted,
+        Scene: "",
+        Logo: "",
+        Name: "",
+        Description: "",
+        Genre: "",
+        Actors: "",
+        Year: "",
+        Duration: "",
+    },
+    {
+        Movie: "",
+        Thumbnail: amerp,
+        Scene: "",
+        Logo: "",
+        Name: "",
+        Description: "",
+        Genre: "",
+        Actors: "",
+        Year: "",
+        Duration: "",
+    },
+    {
+        Movie: "",
+        Thumbnail: tomb,
+        Scene: "",
+        Logo: "",
+        Name: "",
+        Description: "",
+        Genre: "",
+        Actors: "",
+        Year: "",
+        Duration: "",
+    },
+    {
+        Movie: "",
+        Thumbnail: bayw,
+        Scene: "",
+        Logo: "",
+        Name: "",
+        Description: "",
+        Genre: "",
+        Actors: "",
+        Year: "",
+        Duration: "",
+    },
+    {
+        Movie: "",
+        Thumbnail: amaz,
+        Scene: "",
+        Logo: "",
+        Name: "",
+        Description: "",
+        Genre: "",
+        Actors: "",
+        Year: "",
+        Duration: "",
+    },
+    {
+        Movie: "",
+        Thumbnail: hob,
+        Scene: "",
+        Logo: "",
+        Name: "",
+        Description: "",
+        Genre: "",
+        Actors: "",
+        Year: "",
+        Duration: "",
+    },
+    {
+        Movie: "",
+        Thumbnail: lucy,
+        Scene: "",
+        Logo: "",
+        Name: "",
+        Description: "",
+        Genre: "",
+        Actors: "",
+        Year: "",
+        Duration: "",
+    },
+    {
+        Movie: "",
+        Thumbnail: shrek,
+        Scene: "",
+        Logo: "",
+        Name: "",
+        Description: "",
+        Genre: "",
+        Actors: "",
+        Year: "",
+        Duration: "",
+    },    {
+        Movie: "",
+        Thumbnail: reven,
+        Scene: "",
+        Logo: "",
+        Name: "",
+        Description: "",
+        Genre: "",
+        Actors: "",
+        Year: "",
+        Duration: "",
+    },
+];
